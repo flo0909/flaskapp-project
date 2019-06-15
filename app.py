d4ed4d08c03d93f57e11808f132e70f0c604ed03
Florin Palade <florinpalade1@yahoo.co.uk>
2019-06-15 15:46:45 +0100

    app.py modifications

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
