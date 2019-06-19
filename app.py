295a757b3ab503911a31c1c91933d8adb4a7d024
Florin Palade <florinpalade1@yahoo.co.uk>
2019-06-19 16:45:37 +0100

    added Allergens filter

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template,request,redirect,url_for
 from flask_pymongo import PyMongo
 
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 app.config['SECRET_KEY'] = 'secret'
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/index.html')
